8820dc31d5eb759a15bf45f4a675de58d0c466b8
Егор Никитин <Egornik1994@yandex.ru>
2021-07-04 12:31:44 +0300

    Initial Commit

---
+++ StarWarsCharacters/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarWarsCharacters
+//
+//  Created by Егор Никитин on 04.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarWarsCharacters/StarWarsCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsCharactersApp.swift
+//  StarWarsCharacters
+//
+//  Created by Егор Никитин on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
